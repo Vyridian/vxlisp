31546c5a1fb518968a267e75b0e70c844d9089ae
Randall Ratsch <rratsch@gmail.com>
2024-07-27 15:28:07 -0400

    Updating vxlispruntime

--- golang/runcopycore.bat
+++ golang/runcopycore.bat
@@ -3,4 +3,5 @@ xcopy ..\core\vxlisp ..\vxlispruntime\vxlisp /s /y /i
 xcopy ..\core\doc ..\vxlispruntime\doc /s /y /i
 xcopy ..\core\doc\doc.html ..\doc.html /y
 xcopy ..\core\cpp\src\test\resources\testsuite.html ..\testsuite_cpp.html /y
+xcopy ..\core\csharp\test\resources\testsuite.html ..\testsuite_csharp.html /y
 xcopy ..\core\java\src\test\resources\testsuite.html ..\testsuite_java.html /y
